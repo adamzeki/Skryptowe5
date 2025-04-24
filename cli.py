1c5d3c6e7761cdb5ad73294ba9e8ea66ad09d9de
Vilstig <fj.janas@gmail.com>
2025-04-24 12:41:43 +0200

    Small changes in cli. Added main in group_measures.py

--- cli.py
+++ cli.py
@@ -3,6 +3,7 @@ import logging
 import sys
 from datetime import datetime, timedelta
 from cli_utils import print_random_station_generic, mean_and_std_generic
+from group_measures import group_measurement_files_by_key
 
 formatter = logging.Formatter('%(levelname)s: %(message)s')
 
@@ -20,6 +21,7 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(stdout_handler)
 logger.addHandler(stderr_handler)
 
+
 def print_random_station(args):
     print_random_station_generic(args.measure, args.frequency, args.start_date, args.end_date)
 
@@ -29,28 +31,26 @@ def mean_and_std(args):
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="Program do analizy danych stacji pomiarowych.")
+    parser = argparse.ArgumentParser(description="Program for analyzing measurement station data.")
 
-    # Główne argumenty
-    parser.add_argument('-m', '--measure', type=str, required=True, choices=['CO', 'C6H6', 'NO', 'NO2', 'NOx',
-                                                                             'O3', 'PM10', 'PM2.5', 'SO2'],
-
-                        help="Mierzona wielkość")
+    # Main arguments
+    parser.add_argument('-m', '--measure', type=str, required=True, help="Measured parameter")
     parser.add_argument('-f', '--frequency', type=str, required=True, choices=['1g', '24g'],
-                        help="Częstotliwość pomiaru")
+                        help="Measurement frequency")
     parser.add_argument('-s', '--start-date', type=str, required=True,
-                        help="Początek przedziału czasowego w formacie rrrr-mm-dd")
+                        help="Start of the time interval in the format YYYY-MM-DD")
     parser.add_argument('-e', '--end-date', type=str, required=True,
-                        help="Koniec przedziału czasowego w formacie rrrr-mm-dd")
+                        help="End of the time interval in the format YYYY-MM-DD")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     parser_station = subparsers.add_parser('station',
-                                           description="Wypisuje losową stację pomiarową prowadzącą pomiar danej wielkości w danych ramach czasowych")
+                                           description="Prints a random measurement station conducting measurements of the given parameter within the specified time frame")
+
     parser_station.set_defaults(func=print_random_station)
 
     parser_stats = subparsers.add_parser('stats',
-                                         description="Oblicza średnią i odchylenie standardowe dla danej wielkości w zadanym przedziale czasowym dla danej stacji")
+                                         description="Calculates the average and standard deviation for a given parameter within a specified time interval for a given station.")
     parser_stats.add_argument('-s', '--station-code', type=str, required=True, help="Kod stacji")
     parser_stats.set_defaults(func=mean_and_std)
 
@@ -63,15 +63,21 @@ def main():
 
     logger.debug(f"CLI input length: {len(' '.join(sys.argv))} bytes.")
 
+    # Walidacja pomiaru
+    grouped_measures = group_measurement_files_by_key('data_S5/measurements')
+
+    if not any(args.measure in key for key in grouped_measures.keys()):
+        logger.warning(f"No data for measure '{args.measure}'.")
+
     # Walidacja dat
     try:
         args.start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
         args.end_date = datetime.strptime(args.end_date, '%Y-%m-%d') + timedelta(days=1) - timedelta(
             seconds=1)  # Łapiemy cały dzień godzinowo
         if args.start_date > args.end_date:
-            logger.warning("Data początkowa nie może być późniejsza niż data końcowa.")
+            logger.warning("Start date must be before end date.")
     except ValueError:
-        logger.error("Błąd: Niepoprawny format daty. Użyj formatu rrrr-mm-dd.")
+        logger.error("Incorrect data format. Use YYYY-MM-DD.")
         return
 
     # Uruchomienie odpowiedniej funkcji w zależności od podkomendy
